e87501e8b8a59c069279476f3b146baafcac833a
Ryan Andonian <ryanandonian@gmail.com>
2017-04-22 16:13:15 -0700

    Adds player interaction framework for items/tiles. closes randonia/ld38#14

--- dist/js/player.js
+++ dist/js/player.js
@@ -5,6 +5,8 @@ const DIR_LEFT = 4;
 const DIR_RIGHT = 6;
 const CURR_TILE = 5;
 
+var keyAction;
+
 class Player {
   get X() {
     return this.sprite.body.position.x;
@@ -23,6 +25,19 @@ class Player {
     game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
     this.sprite.body.collideWorldBounds = true;
     this.directions = {};
+    this.facingDirection = DIR_DOWN;
+    keyAction = game.input.keyboard.addKey(Phaser.Keyboard.F);
+    keyAction.onDown.add(this.actionHandler, this);
+  }
+  actionHandler(event) {
+    switch (event.keyCode) {
+      case Phaser.Keyboard.F:
+        this.doFishAction(event);
+        break;
+    }
+  }
+  doFishAction(event) {
+    console.log(this.adjacentTiles[this.facingDirection].habitat);
   }
   update() {
     this.directions[DIR_UP] = cursors.up.isDown;
@@ -35,15 +50,19 @@ class Player {
     }
     if (this.directions[DIR_UP]) {
       dir.y -= 1;
+      this.facingDirection = DIR_UP;
     }
     if (this.directions[DIR_DOWN]) {
       dir.y += 1;
+      this.facingDirection = DIR_DOWN;
     }
     if (this.directions[DIR_LEFT]) {
       dir.x -= 1;
+      this.facingDirection = DIR_LEFT;
     }
     if (this.directions[DIR_RIGHT]) {
       dir.x += 1;
+      this.facingDirection = DIR_RIGHT;
     }
 
     this.sprite.body.velocity.x = dir.x * PLAYER_SPEED;
@@ -56,23 +75,23 @@ class Player {
     drawText(0, HEIGHT - 20, sprintf('Player Position: {%d, %d}', this.X, this.Y), textOpts);
     var tileX = layer.getTileX(this.X);
     var tileY = layer.getTileY(this.Y);
-    var tiles = {
-      DIR_LEFT: map.getTileLeft(0, tileX, tileY),
-      DIR_RIGHT: map.getTileRight(0, tileX, tileY),
-      DIR_UP: map.getTileAbove(0, tileX, tileY),
-      DIR_DOWN: map.getTileBelow(0, tileX, tileY),
-      CURR_TILE: map.getTile(tileX, tileY, 0)
-    }
-    var l = (tiles.DIR_LEFT) ? tiles.DIR_LEFT.index : -1;
-    var lH = (tiles.DIR_LEFT) ? tiles.DIR_LEFT.habitat : -1;
-    var r = (tiles.DIR_RIGHT) ? tiles.DIR_RIGHT.index : -1;
-    var rH = (tiles.DIR_RIGHT) ? tiles.DIR_RIGHT.habitat : -1;
-    var a = (tiles.DIR_UP) ? tiles.DIR_UP.index : -1;
-    var aH = (tiles.DIR_UP) ? tiles.DIR_UP.habitat : -1;
-    var b = (tiles.DIR_DOWN) ? tiles.DIR_DOWN.index : -1;
-    var bH = (tiles.DIR_DOWN) ? tiles.DIR_DOWN.habitat : -1;
-    var c = (tiles.CURR_TILE) ? tiles.CURR_TILE.index : -1;
-    var cH = (tiles.CURR_TILE) ? tiles.CURR_TILE.habitat : -1;
+    var tiles = {};
+    tiles[DIR_LEFT] = map.getTileLeft(0, tileX, tileY);
+    tiles[DIR_RIGHT] = map.getTileRight(0, tileX, tileY);
+    tiles[DIR_UP] = map.getTileAbove(0, tileX, tileY);
+    tiles[DIR_DOWN] = map.getTileBelow(0, tileX, tileY);
+    tiles[CURR_TILE] = map.getTile(tileX, tileY, 0);
+    this.adjacentTiles = tiles;
+    var l = (tiles[DIR_LEFT]) ? tiles[DIR_LEFT].index : -1;
+    var lH = (tiles[DIR_LEFT]) ? tiles[DIR_LEFT].habitat : -1;
+    var r = (tiles[DIR_RIGHT]) ? tiles[DIR_RIGHT].index : -1;
+    var rH = (tiles[DIR_RIGHT]) ? tiles[DIR_RIGHT].habitat : -1;
+    var a = (tiles[DIR_UP]) ? tiles[DIR_UP].index : -1;
+    var aH = (tiles[DIR_UP]) ? tiles[DIR_UP].habitat : -1;
+    var b = (tiles[DIR_DOWN]) ? tiles[DIR_DOWN].index : -1;
+    var bH = (tiles[DIR_DOWN]) ? tiles[DIR_DOWN].habitat : -1;
+    var c = (tiles[CURR_TILE]) ? tiles[CURR_TILE].index : -1;
+    var cH = (tiles[CURR_TILE]) ? tiles[CURR_TILE].habitat : -1;
     drawText(0, HEIGHT - 10, sprintf('L: %s R: %s U: %s D: %s C: %s', l, r, a, b, c));
     drawText(0, HEIGHT, sprintf('L: %s R: %s U: %s D: %s', lH, rH, aH, bH));
   }
